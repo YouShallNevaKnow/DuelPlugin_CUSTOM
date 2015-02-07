44b977a43be8e1ac9cfb8292f59828f4614eb460
gogobebe2 <gogobebe2@gmail.com>
2015-02-07 21:00:55 +1300

    stuff

--- src/com/gmail/gogobebe2/duel/Duel.java
+++ src/com/gmail/gogobebe2/duel/Duel.java
@@ -1,7 +1,8 @@
 package com.gmail.gogobebe2.duel;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,41 +12,50 @@ public class Duel extends JavaPlugin {
     @Override
     public void onEnable() {
         loadConf();
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new DuelListener(this), this);
     }
 
     private void loadConf() {
         this.getLogger().info("Loading config");
         if (!new File(this.getDataFolder(), "config.yml").exists()) {
             this.saveDefaultConfig();
-            reloadConfig();
-            if (!Bukkit.getOnlinePlayers().isEmpty()) {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    storePlayerInfo(player);
-                }
-            }
         }
         reloadConfig();
     }
 
-    private void storePlayerInfo(Player player) {
-        try {
-            int wins = this.getConfig().getInt("Players." + player.getUniqueId() + ".wins");
-            int loses = this.getConfig().getInt("Players." + player.getUniqueId() + ".loses");
-            if ((wins == 0) && (loses == 0)) {
-                throw new Exception();
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (commandLabel.equalsIgnoreCase("duel")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "You have to be a player to use this command!");
+                return true;
             }
-            player.sendMessage(ChatColor.AQUA + "You currently have " + ChatColor.GREEN + wins + ChatColor.AQUA + " wins and "
-                    + ChatColor.GREEN + loses + ChatColor.AQUA + " loses. Go duel more people with /duel");
-        } catch (Exception except) {
-            this.getConfig().set("Players." + player.getUniqueId() + ".wins", 0);
-            this.getConfig().set("Players." + player.getUniqueId() + ".loses", 0);
-            saveConfig();
-            player.sendMessage(ChatColor.DARK_GREEN + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + "Welcome " + ChatColor.DARK_GREEN + "" + ChatColor.ITALIC
-                    + player.getDisplayName() + ChatColor.DARK_GREEN + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + "! Checkout our new plugin! Type /duel to get started.");
-            //noinspection SpellCheckingInspection
-            player.sendMessage(ChatColor.AQUA + "Regards, William Bryant (A.K.A. gogobebe2)" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD + " - Plugin developer)");
-        }
-    }
+            Player player = (Player) sender;
 
+
+            if (args.length == 0) {
+
+                //Requesting
+
+                return true;
+            }
+            else if (args[0].equalsIgnoreCase("accept")) {
+                if (args.length > 1) {
+                    player.sendMessage(ChatColor.GOLD + "Too many arguments! Type " + ChatColor.GREEN + ChatColor.ITALIC
+                            + "/duel <player>" + ChatColor.GOLD + " or " + ChatColor.GREEN + ChatColor.ITALIC + "/duel accept");
+                    return true;
+                }
+
+                //Accepting
+
+                return true;
+            }
+            else {
+                player.sendMessage(ChatColor.GOLD + "Wrong args! Type " + ChatColor.GREEN + ChatColor.ITALIC
+                        + "/duel <player>" + ChatColor.GOLD + " or " + ChatColor.GREEN + ChatColor.ITALIC + "/duel accept");
+                return true;
+            }
+        }
+        return true;
+    }
 }
--- src/com/gmail/gogobebe2/duel/DuelListener.java
+++ src/com/gmail/gogobebe2/duel/DuelListener.java
@@ -1,7 +1,15 @@
 package com.gmail.gogobebe2.duel;
 
+import org.bukkit.event.Listener;
+
 /**
  * Created by william on 2/7/15.
  */
-public class DuelListener {
+public class DuelListener implements Listener{
+    private final Duel plugin;
+
+    public DuelListener(Duel plugin) {
+        this.plugin = plugin; // Store the plugin in situations where you need it.
+    }
+
 }
