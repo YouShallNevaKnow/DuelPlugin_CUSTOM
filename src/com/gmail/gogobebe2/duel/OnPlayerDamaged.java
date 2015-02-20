c660e6dd68a3c397396ec8f9173b81f40f482f46
gogobebe2 <gogobebe2@gmail.com>
2015-02-20 21:28:33 +1300

    version: 0.8.2_CUSTOM_DEV Fixed onPlayerDamaged event again.

--- src/com/gmail/gogobebe2/duel/OnPlayerDamaged.java
+++ src/com/gmail/gogobebe2/duel/OnPlayerDamaged.java
@@ -26,14 +26,14 @@ public class OnPlayerDamaged implements Listener {
                         } else if (players[1].equals(player) && players[0].equals(eEvent.getDamager())) {
                             killer = players[0];
                         } else if (players[0].equals(eEvent.getDamager()) || players[1].equals(eEvent.getDamager())) {
-                            if (!(eEvent.getDamager() instanceof Player)) {
+                            if (eEvent.getDamager() instanceof Player) {
                                 killer = (Player) eEvent.getDamager();
                                 killer.sendMessage(ChatColor.BLUE + "You cannot damage people who aren't in the duel.");
                             }
                             event.setCancelled(true);
                             return;
                         } else if (players[0].equals(player) || players[1].equals(player)) {
-                            if (!(eEvent.getDamager() instanceof Player)) {
+                            if (eEvent.getDamager() instanceof Player) {
                                 killer = (Player) eEvent.getDamager();
                                 killer.sendMessage(ChatColor.BLUE + "You cannot damage people who are in a duel!");
                             }
